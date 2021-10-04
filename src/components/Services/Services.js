8fb0d60145f8bcb336bf4c5f5f80f2eeab63ac50
mynulislam75 <mynulislam181120@gmail.com>
2021-10-04 21:09:50 +0600

    modify text

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,30 +2,33 @@ import React, { useEffect, useState } from 'react';
 import './Services.css'
 
 const Services = () => {
-    const [services,setServices]=useState([])
-    useEffect(()=>{
+    const [services, setServices] = useState([])
+    useEffect(() => {
         fetch('./services.JSON')
-        .then(res=>res.json())
-        .then(data=>setServices(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     console.log(services)
     return (
         <div className="row">
-           {
-               services.map(service=>
-                
-                <div className="col-lg-4" id="service-container">
-                    <img src={service.img} className="img-fluid" alt=""/>
-                   <h3>
-                   {service.name}
-                    </h3>
-                    <h5>{service.teacher}</h5>
-                <p>Price:{service.price}</p>
-                   
-                </div>
-                
+            {
+                services.map(service =>
+
+                    <div className="col-lg-4 container" id="service-container">
+                        <img src={service.img} className="img-fluid p-2" alt="" />
+
+                        <div className="p-3">
+                        <h3>
+                            {service.name}
+                        </h3>
+                        <h6>Instructor:{service.teacher}</h6>
+                        <p>Price:${service.price}</p>
+                        </div>
+
+                    </div>
+
                 )
-           }
+            }
         </div>
     );
 };
--- src/components/Singlecourse/Singlecourse.js
+++ src/components/Singlecourse/Singlecourse.js
@@ -7,11 +7,12 @@ const Singlecourse = (props) => {
         <div className="col-lg-2 ps-5 pe-5" id="course-details">
 
             
-            <img className="img-fluid w-100 h-50 p-1" src={img} alt="" />
+            <img className="img-fluid p-2" src={img} alt="" />
             <div className="p-2">
             <h3>{name}</h3>
             <h3>{teacher}</h3>
             <h5>price:{price}</h5>
+            <button className="bg-primary">Enroll Now</button>
             </div>
           
         
