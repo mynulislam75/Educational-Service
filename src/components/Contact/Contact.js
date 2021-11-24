13c3b367038e77b8a45f708ec64185b8333a630a
mynulislam75 <mynulislam181120@gmail.com>
2021-11-24 20:35:46 +0600

    adding some text

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className="d-flex justify-content-center">
-            <h3>CONTACT COMING SOON....</h3>
+            <h3>CONTACT COMING SOON.......</h3>
         </div>
     );
 };
